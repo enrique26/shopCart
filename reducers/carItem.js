af20842f14dec842d85f082d7ab734aff96660d6
enrique26 <enriqueibilis@gmail.com>
2019-08-14 17:31:08 -0500

    se cambia la interacion con el objeto para actualizar el state

--- reducers/carItem.js
+++ reducers/carItem.js
@@ -1,10 +1,13 @@
 const carItems = (state=[], action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state,action.payload]
+      return {cartItems:[...state.cartItems,action.payload]}
       break;
     case 'REMOVE_FROM_CART':
-      return state.filter(cartItem=>cartItem.id !== action.payload.id)
+      return {
+        cartItems:state.cartItems.filter(cartItem=>cartItem.id !== action.payload.id)
+      }
+
       break;
     default:
       return state
